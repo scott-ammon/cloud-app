67203fd9606e08d4b711d926a9c20baa0ba9f463
Scott Ammon <scott.o.ammon@gmail.com>
2018-06-13 14:57:55 -0700

    added cloudinary upload functionality

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 var express = require('express');
+
 require('dotenv').config();
 
 var path = require('path');
 var app = express();
 var ejsLayouts = require('express-ejs-layouts');
+var multer = require('multer');
+// referencing the uploads folder
+var upload = multer({dest: "./uploads/"})
+var cloud = require('cloudinary');
 
 var port = process.env.PORT || 2000;
 
-app.use(express.static(path.join(__dirname, 'static')));
+// app.use(express.static(path.join(__dirname, 'static')));
 
 app.set('view engine', 'ejs');
 app.use(ejsLayouts);
@@ -21,8 +26,12 @@ app.get('/upload', function(req, res) {
 	res.render('upload');
 });
 
-app.post('/upload', function(req, res) {
+// 'myFile' references the name attribute in the form 
+app.post('/upload', upload.single('myFile'), function(req, res) {
   console.log('post route hit!');
+  cloud.uploader.upload(req.file.path, function(result) {
+    console.log(result);
+  });
 });
 
 app.listen(port, function() {
